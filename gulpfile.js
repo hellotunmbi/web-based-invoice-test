4f3c8893b1a8cc9e432c6f529fbd096b0aa32c00
Hello Tunmbi <hellotunmbi@gmail.com>
2017-04-25 21:56:43 +0100

    added comments

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ gulp.task('build-less', function() {
 });
 
 
+// Build and Compile all less files into assets/css folder
 gulp.task('build-css', function() {
     return gulp.src('src/less/*.less')
         .pipe(sourcemaps.init())
@@ -34,6 +35,8 @@ gulp.task('build-css', function() {
         .pipe(gulp.dest('assets/css'));
 });
 
+
+// Uglify and add up all javascripts files (concat) into assets/js folder
 
 gulp.task('build-js', function() {
     return gulp.src('src/javascript/*.js')
@@ -49,7 +52,7 @@ gulp.task('html', function() {
   .pipe(connect.reload())
 });
 
-
+// Set up Live reload
 gulp.task('connect', function() {
   connect.server({
     root: '.',
@@ -57,6 +60,7 @@ gulp.task('connect', function() {
   })
 });
 
+// Keep an eye on all the tasks
 gulp.task('watch', function() {
     gulp.watch('src/javascript/*.js', ['jshint']);
     gulp.watch('src/less/*.less', ['build-less'])
